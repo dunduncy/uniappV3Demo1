35b5c497af1b2ecbca936924fde57d627638f225
MYWIND\117072 <dun.caiyun@relectric.cn>
2025-10-17 14:23:50 +0800

    no message

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,26 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="layout">
+		<view class="box1">
+			box1
+		</view>
+		<view class="box2">
+			box2
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script setup>
+	
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+<style lang="scss">
+	.layout{
+		border: 1px solid red;
+		.box1{
+			border: 1px solid green;
+		}
+		.box2{
+			border: 1px solid blue;
+		}
 	}
 </style>
